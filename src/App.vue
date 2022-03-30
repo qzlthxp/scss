bd16920a153a9f8ea4802c4b5fd2547e5901c4fd
qzlthxp <2902587206@qq.com>
2022-03-30 18:00:01 +0800

    edit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="content">
+    <h1>hi</h1>
+    <p>paragraph</p>
+    <article>
+      <a href="">
+        baidu
+        <img src="" alt="123" />
+      </a>
+    </article>
+  </div>
 </template>
 
 <script>
@@ -14,13 +24,17 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../src/styles/1';
+@import '../src/styles/5';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $plain-font;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: $app;
+  color: $highlight-color;
   margin-top: 60px;
+  @include rounded-corners;
+  background-color: darkblue;
 }
 </style>
